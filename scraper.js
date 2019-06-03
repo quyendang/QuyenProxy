9e2bc98b06ac662ace7ef5daab0d3647af8c24df
Quyen Dang <dangnquyen@hotmail.com>
2019-06-03 18:07:04 +0700

    aa

--- scraper.js
+++ scraper.js
@@ -1,20 +1,3 @@
-// var list = await proxy.GetTxtProxy("http://filefab.com/api.php?l=PmxKWgtrKlgMeJa2Z7PTStV5Dgr7Kkn57WZX8lchXd0");
-//                
-//                 var list1 = await proxy.GetTxtProxy("http://rootjazz.com/proxies/proxies.txt");
-//                 var list2 = await proxy.GetHtmlProxy("http://proxyserverlist-24.blogspot.com/feeds/posts/default");
-//                 var list3 = await proxy.GetHtmlProxy("http://sslproxies24.blogspot.in/feeds/posts/default");
-//                 var list4 = await proxy.GetHtmlProxy("http://www.live-socks.net/feeds/posts/default");
-//                 var list5 = await proxy.GetHtmlProxy("http://proxyape.com/");
-//                 var list6 = await proxy.GetHtmlProxy("http://sock5us.blogspot.com/");
-//                 var list7 = await proxy.GetHtmlProxy("https://anotepad.com/notes/aa3fj6");
-//                 var list8 = await proxy.GetHtmlProxy("http://proxy-daily.com/");
-//                 var list9 = await proxy.GetHtmlProxy("https://www.dailyfreeproxy.com/feeds/posts/default");
-//                 var list10 = await proxy.GetTxtProxy("https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks.txt");
-//                 var list11 = await proxy.GetTxtProxy("https://api.proxyscrape.com/?request=getproxies&proxytype=http&timeout=10000&country=all&ssl=all&anonymity=all");
-//                 var list12 = await proxy.GetTxtProxy("https://api.proxyscrape.com/?request=getproxies&proxytype=socks4&timeout=10000&country=all");
-//                 var list13 = await proxy.GetTxtProxy("https://api.proxyscrape.com/?request=getproxies&proxytype=socks5&timeout=10000&country=all");
-
-
 console.log("Hello!");
 
 var util = require("util");
@@ -26,18 +9,18 @@ function initDatabase(callback) {
 	var db = new sqlite3.Database("data.sqlite");
 	db.serialize(function() {
 		//db.run("DROP TABLE IF EXISTS data");
-		db.run("CREATE TABLE IF NOT EXISTS data (ip TEXT PRIMARY KEY, port INT, type TEXT, code TEXT, country TEXT, anonymity TEXT, google TEXT, https TEXT, lastchecked TEXT)");
+		db.run("CREATE TABLE IF NOT EXISTS data (code TEXT PRIMARY KEY, link TEXT)");
 		callback(db);
 	});
 }
 
-function updateRow(db, ip, port, code, country, anonymity, google, https, lastchecked, type) {
-	var statementIn = db.prepare("INSERT OR IGNORE INTO data VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
-	statementIn.run(ip, port, type, code, country, anonymity, google, https, lastchecked);
+function updateRow(db, code, link) {
+	var statementIn = db.prepare("INSERT OR IGNORE INTO data VALUES (?, ?)");
+	statementIn.run(code, link);
 	statementIn.finalize();
 
-	var statementUp = db.prepare("UPDATE data SET port = ?, type = ?, code = ?, country = ?, anonymity = ?, google = ?, https = ?, lastchecked = ? WHERE ip LIKE ?");
-	statementUp.run(port, type, code, country, anonymity, google, https, lastchecked, ip);
+	var statementUp = db.prepare("UPDATE data SET link = ? WHERE code LIKE ?");
+	statementUp.run(link, code);
 	statementUp.finalize();
 }
 
@@ -47,9 +30,9 @@ function pad(str, max, char) {
 }
 
 function readRows(db) {
-	db.each("SELECT rowid AS id, ip, port, type, lastchecked FROM data", function(err, row) {
+	db.each("SELECT rowid AS code, link FROM data", function(err, row) {
 		if (err) return console.log("Error", util.inspect(err));
-		console.log("[" + pad(row.id.toString(), 4, "0") + "] " + pad(row.type, 9, " ") + " - " + pad(row.ip, 15, " ") + ":" + row.port.toString() + (new Array(Math.max(6 - row.port.toString().length, 0)).join(" ")) + " - " + new Date(row.lastchecked).toGMTString());
+		//console.log("[" + pad(row.id.toString(), 4, "0") + "] " + pad(row.type, 9, " ") + " - " + pad(row.ip, 15, " ") + ":" + row.port.toString() + (new Array(Math.max(6 - row.port.toString().length, 0)).join(" ")) + " - " + new Date(row.lastchecked).toGMTString());
 	});
 }
 
@@ -64,8 +47,8 @@ function cleanUp(db) {
 				var d = new Date(row.lastchecked);
 				var r;
 				if (!(r = (d.setDate(d.getDate() + 7)) >= new Date())) {
-					console.log("Removed proxy", pad(row.type, 9, " ") + " - " + pad(row.ip, 15, " ") + ":" + row.port.toString() + (new Array(Math.max(6 - row.port.toString().length, 0)).join(" ")) + " - " + new Date(row.lastchecked).toGMTString());
-					db.run("DELETE FROM data WHERE ip = ?", row.ip);
+					//console.log("Removed proxy", pad(row.type, 9, " ") + " - " + pad(row.ip, 15, " ") + ":" + row.port.toString() + (new Array(Math.max(6 - row.port.toString().length, 0)).join(" ")) + " - " + new Date(row.lastchecked).toGMTString());
+					db.run("DELETE FROM data WHERE code = ?", row.code);
 				}
 				return r;
 			});
@@ -75,7 +58,13 @@ function cleanUp(db) {
 }
 
 function fetchPage(url, callback) {
-	request(url, function(error, response, body) {
+	var options = {
+  		url: url,
+  		headers: {
+    		'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.1 Safari/605.1.15'
+  		}
+	};
+	request(options, function(error, response, body) {
 		if (error) {
 			console.log("Error requesting page: " + error);
 			return;
@@ -85,109 +74,15 @@ function fetchPage(url, callback) {
 	});
 }
 
-function scrapperTxt(db, site, type){
+
+
+function scrapper(db, site, code) {
 	return new Promise(function(resolve, reject) {
 		fetchPage(site, function(body) {
-			var elements = body.split("\n");
+			var linkData = body.split('?bp=').pop().split('","')[0];
 			console.log()
-			console.log("Found " + elements.length + " elements");
-			for (var i = 0, len = elements.length; i < len; i++) {
-				var item = elements[i].split(":");
-				var ip = item[0];
-				var port = parseInt(item[1], 10);
-				var code = "";
-				var country = "";
-				var anonymity = "";
-				var google = "";
-				var https = "";
-
-				var d = new Date();
-				var lastchecked = d.toJSON();
-
-				updateRow(db, ip, port, code, country, anonymity, google, https, lastchecked, type);
-			}
-			resolve();
-		});
-	});
-}
-
-
-function scrapperHtml(db, site, type){
-	return new Promise(function(resolve, reject) {
-		fetchPage(site, function(body) {
-			var regex = /[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}:[0-9]{1,5}/ig;
-			var elements = body.match(regex);
-			console.log()
-			console.log("Found " + elements.length + " elements");
-			for (var i = 0, len = elements.length; i < len; i++) {
-				try {
-					var item = elements[i].toString().split(":");
-					var ip = item[0].toString();
-					var port = parseInt(item[1], 10);
-					var code = "";
-					var country = "";
-					var anonymity = "";
-					var google = "";
-					var https = "";
-
-					var d = new Date();
-					var lastchecked = d.toJSON();
-
-					updateRow(db, ip, port, code, country, anonymity, google, https, lastchecked, type);
-				}
-				catch(error) {
-  					console.error(error);
-				}
-			}
-			resolve();
-		});
-	});
-}
-
-
-
-function scrapper(db, site, type) {
-	return new Promise(function(resolve, reject) {
-		fetchPage(site, function(body) {
-			var $ = cheerio.load(body);
-
-			var elements = $("#proxylisttable > tbody > tr");
-			console.log()
-			console.log("Found " + elements.length + " elements");
-			elements.each(function() {
-				var item = $(this).find("td");
-
-				var ip = $(item[0]).text();
-				var port = parseInt($(item[1]).text(), 10);
-				var code = $(item[2]).text();
-				var country = $(item[3]).text();
-				var anonymity = $(item[4]).text();
-				var google = $(item[5]).text();
-				var https = $(item[6]).text();
-
-				var d = new Date();
-				var reg = /(1 day|\d+? days)? ?(1 hour|\d+? hours)? ?(1 minute|\d+? minutes)? ?(1 second|\d+? seconds)? ago/i;
-				var last = $(item[7]).text();
-				if (reg.test(last)) {
-					var matches = last.match(reg);
-					if (matches[1] != undefined) {
-						d.setDate(d.getDate() - parseInt(matches[1], 10));
-					}
-					if (matches[2] != undefined) {
-						d.setHours(d.getHours() - parseInt(matches[2], 10));
-					}
-					if (matches[3] != undefined) {
-						d.setMinutes(d.getMinutes() - parseInt(matches[3], 10));
-					}
-					if (matches[4] != undefined) {
-						d.setSeconds(d.getSeconds() - parseInt(matches[4], 10));
-					}
-				}
-
-				var lastchecked = d.toJSON();
-
-				updateRow(db, ip, port, code, country, anonymity, google, https, lastchecked, type);
-			});
+			console.log(linkData);
+			updateRow(db, code, linkData);
 			resolve();
 		});
 	});
@@ -195,16 +90,12 @@ function scrapper(db, site, type) {
 
 function run(db) {
 	var scrappers = [];
-	//scrappers.push(scrapper(db, "http://free-proxy-list.net/", "free"));
-	//skipped web proxies
-	//scrappers.push(scrapper(db, "http://sslproxies.org/", "ssl"));
-	//scrappers.push(scrapper(db, "http://us-proxy.org/", "us"));
-	//scrappers.push(scrapper(db, "http://free-proxy-list.net/uk-proxy.html", "uk"));
-	//skipped socks4/socks5
-	//scrappers.push(scrapper(db, "http://google-proxy.net/", "google"));
-	//scrappers.push(scrapper(db, "http://free-proxy-list.net/anonymous-proxy.html", "anonymous"));
-	//scrappers.push(scrapperTxt(db, "http://filefab.com/api.php?l=PmxKWgtrKlgMeJa2Z7PTStV5Dgr7Kkn57WZX8lchXd0", "free"));
-	scrappers.push(scrapperHtml(db, "https://anotepad.com/notes/aa3fj6", "free"));
+	var json = '[{"Code": "AF", "Name": "Afghanistan"},{"Code": "AX", "Name": "\u00c5land Islands"},{"Code": "AL", "Name": "Albania"},{"Code": "DZ", "Name": "Algeria"},{"Code": "AS", "Name": "American Samoa"},{"Code": "AD", "Name": "Andorra"},{"Code": "AO", "Name": "Angola"},{"Code": "AI", "Name": "Anguilla"},{"Code": "AQ", "Name": "Antarctica"},{"Code": "AG", "Name": "Antigua and Barbuda"},{"Code": "AR", "Name": "Argentina"},{"Code": "AM", "Name": "Armenia"},{"Code": "AW", "Name": "Aruba"},{"Code": "AU", "Name": "Australia"},{"Code": "AT", "Name": "Austria"},{"Code": "AZ", "Name": "Azerbaijan"},{"Code": "BS", "Name": "Bahamas"},{"Code": "BH", "Name": "Bahrain"},{"Code": "BD", "Name": "Bangladesh"},{"Code": "BB", "Name": "Barbados"},{"Code": "BY", "Name": "Belarus"},{"Code": "BE", "Name": "Belgium"},{"Code": "BZ", "Name": "Belize"},{"Code": "BJ", "Name": "Benin"},{"Code": "BM", "Name": "Bermuda"},{"Code": "BT", "Name": "Bhutan"},{"Code": "BO", "Name": "Bolivia, Plurinational State of"},{"Code": "BQ", "Name": "Bonaire, Sint Eustatius and Saba"},{"Code": "BA", "Name": "Bosnia and Herzegovina"},{"Code": "BW", "Name": "Botswana"},{"Code": "BV", "Name": "Bouvet Island"},{"Code": "BR", "Name": "Brazil"},{"Code": "IO", "Name": "British Indian Ocean Territory"},{"Code": "BN", "Name": "Brunei Darussalam"},{"Code": "BG", "Name": "Bulgaria"},{"Code": "BF", "Name": "Burkina Faso"},{"Code": "BI", "Name": "Burundi"},{"Code": "KH", "Name": "Cambodia"},{"Code": "CM", "Name": "Cameroon"},{"Code": "CA", "Name": "Canada"},{"Code": "CV", "Name": "Cape Verde"},{"Code": "KY", "Name": "Cayman Islands"},{"Code": "CF", "Name": "Central African Republic"},{"Code": "TD", "Name": "Chad"},{"Code": "CL", "Name": "Chile"},{"Code": "CN", "Name": "China"},{"Code": "CX", "Name": "Christmas Island"},{"Code": "CC", "Name": "Cocos (Keeling) Islands"},{"Code": "CO", "Name": "Colombia"},{"Code": "KM", "Name": "Comoros"},{"Code": "CG", "Name": "Congo"},{"Code": "CD", "Name": "Congo, the Democratic Republic of the"},{"Code": "CK", "Name": "Cook Islands"},{"Code": "CR", "Name": "Costa Rica"},{"Code": "CI", "Name": "C\u00f4te d'Ivoire"},{"Code": "HR", "Name": "Croatia"},{"Code": "CU", "Name": "Cuba"},{"Code": "CW", "Name": "Cura\u00e7ao"},{"Code": "CY", "Name": "Cyprus"},{"Code": "CZ", "Name": "Czech Republic"},{"Code": "DK", "Name": "Denmark"},{"Code": "DJ", "Name": "Djibouti"},{"Code": "DM", "Name": "Dominica"},{"Code": "DO", "Name": "Dominican Republic"},{"Code": "EC", "Name": "Ecuador"},{"Code": "EG", "Name": "Egypt"},{"Code": "SV", "Name": "El Salvador"},{"Code": "GQ", "Name": "Equatorial Guinea"},{"Code": "ER", "Name": "Eritrea"},{"Code": "EE", "Name": "Estonia"},{"Code": "ET", "Name": "Ethiopia"},{"Code": "FK", "Name": "Falkland Islands (Malvinas)"},{"Code": "FO", "Name": "Faroe Islands"},{"Code": "FJ", "Name": "Fiji"},{"Code": "FI", "Name": "Finland"},{"Code": "FR", "Name": "France"},{"Code": "GF", "Name": "French Guiana"},{"Code": "PF", "Name": "French Polynesia"},{"Code": "TF", "Name": "French Southern Territories"},{"Code": "GA", "Name": "Gabon"},{"Code": "GM", "Name": "Gambia"},{"Code": "GE", "Name": "Georgia"},{"Code": "DE", "Name": "Germany"},{"Code": "GH", "Name": "Ghana"},{"Code": "GI", "Name": "Gibraltar"},{"Code": "GR", "Name": "Greece"},{"Code": "GL", "Name": "Greenland"},{"Code": "GD", "Name": "Grenada"},{"Code": "GP", "Name": "Guadeloupe"},{"Code": "GU", "Name": "Guam"},{"Code": "GT", "Name": "Guatemala"},{"Code": "GG", "Name": "Guernsey"},{"Code": "GN", "Name": "Guinea"},{"Code": "GW", "Name": "Guinea-Bissau"},{"Code": "GY", "Name": "Guyana"},{"Code": "HT", "Name": "Haiti"},{"Code": "HM", "Name": "Heard Island and McDonald Islands"},{"Code": "VA", "Name": "Holy See (Vatican City State)"},{"Code": "HN", "Name": "Honduras"},{"Code": "HK", "Name": "Hong Kong"},{"Code": "HU", "Name": "Hungary"},{"Code": "IS", "Name": "Iceland"},{"Code": "IN", "Name": "India"},{"Code": "ID", "Name": "Indonesia"},{"Code": "IR", "Name": "Iran, Islamic Republic of"},{"Code": "IQ", "Name": "Iraq"},{"Code": "IE", "Name": "Ireland"},{"Code": "IM", "Name": "Isle of Man"},{"Code": "IL", "Name": "Israel"},{"Code": "IT", "Name": "Italy"},{"Code": "JM", "Name": "Jamaica"},{"Code": "JP", "Name": "Japan"},{"Code": "JE", "Name": "Jersey"},{"Code": "JO", "Name": "Jordan"},{"Code": "KZ", "Name": "Kazakhstan"},{"Code": "KE", "Name": "Kenya"},{"Code": "KI", "Name": "Kiribati"},{"Code": "KP", "Name": "Korea, Democratic People's Republic of"},{"Code": "KR", "Name": "Korea, Republic of"},{"Code": "KW", "Name": "Kuwait"},{"Code": "KG", "Name": "Kyrgyzstan"},{"Code": "LA", "Name": "Lao People's Democratic Republic"},{"Code": "LV", "Name": "Latvia"},{"Code": "LB", "Name": "Lebanon"},{"Code": "LS", "Name": "Lesotho"},{"Code": "LR", "Name": "Liberia"},{"Code": "LY", "Name": "Libya"},{"Code": "LI", "Name": "Liechtenstein"},{"Code": "LT", "Name": "Lithuania"},{"Code": "LU", "Name": "Luxembourg"},{"Code": "MO", "Name": "Macao"},{"Code": "MK", "Name": "Macedonia, the Former Yugoslav Republic of"},{"Code": "MG", "Name": "Madagascar"},{"Code": "MW", "Name": "Malawi"},{"Code": "MY", "Name": "Malaysia"},{"Code": "MV", "Name": "Maldives"},{"Code": "ML", "Name": "Mali"},{"Code": "MT", "Name": "Malta"},{"Code": "MH", "Name": "Marshall Islands"},{"Code": "MQ", "Name": "Martinique"},{"Code": "MR", "Name": "Mauritania"},{"Code": "MU", "Name": "Mauritius"},{"Code": "YT", "Name": "Mayotte"},{"Code": "MX", "Name": "Mexico"},{"Code": "FM", "Name": "Micronesia, Federated States of"},{"Code": "MD", "Name": "Moldova, Republic of"},{"Code": "MC", "Name": "Monaco"},{"Code": "MN", "Name": "Mongolia"},{"Code": "ME", "Name": "Montenegro"},{"Code": "MS", "Name": "Montserrat"},{"Code": "MA", "Name": "Morocco"},{"Code": "MZ", "Name": "Mozambique"},{"Code": "MM", "Name": "Myanmar"},{"Code": "NA", "Name": "Namibia"},{"Code": "NR", "Name": "Nauru"},{"Code": "NP", "Name": "Nepal"},{"Code": "NL", "Name": "Netherlands"},{"Code": "NC", "Name": "New Caledonia"},{"Code": "NZ", "Name": "New Zealand"},{"Code": "NI", "Name": "Nicaragua"},{"Code": "NE", "Name": "Niger"},{"Code": "NG", "Name": "Nigeria"},{"Code": "NU", "Name": "Niue"},{"Code": "NF", "Name": "Norfolk Island"},{"Code": "MP", "Name": "Northern Mariana Islands"},{"Code": "NO", "Name": "Norway"},{"Code": "OM", "Name": "Oman"},{"Code": "PK", "Name": "Pakistan"},{"Code": "PW", "Name": "Palau"},{"Code": "PS", "Name": "Palestine, State of"},{"Code": "PA", "Name": "Panama"},{"Code": "PG", "Name": "Papua New Guinea"},{"Code": "PY", "Name": "Paraguay"},{"Code": "PE", "Name": "Peru"},{"Code": "PH", "Name": "Philippines"},{"Code": "PN", "Name": "Pitcairn"},{"Code": "PL", "Name": "Poland"},{"Code": "PT", "Name": "Portugal"},{"Code": "PR", "Name": "Puerto Rico"},{"Code": "QA", "Name": "Qatar"},{"Code": "RE", "Name": "R\u00e9union"},{"Code": "RO", "Name": "Romania"},{"Code": "RU", "Name": "Russian Federation"},{"Code": "RW", "Name": "Rwanda"},{"Code": "BL", "Name": "Saint Barth\u00e9lemy"},{"Code": "SH", "Name": "Saint Helena, Ascension and Tristan da Cunha"},{"Code": "KN", "Name": "Saint Kitts and Nevis"},{"Code": "LC", "Name": "Saint Lucia"},{"Code": "MF", "Name": "Saint Martin (French part)"},{"Code": "PM", "Name": "Saint Pierre and Miquelon"},{"Code": "VC", "Name": "Saint Vincent and the Grenadines"},{"Code": "WS", "Name": "Samoa"},{"Code": "SM", "Name": "San Marino"},{"Code": "ST", "Name": "Sao Tome and Principe"},{"Code": "SA", "Name": "Saudi Arabia"},{"Code": "SN", "Name": "Senegal"},{"Code": "RS", "Name": "Serbia"},{"Code": "SC", "Name": "Seychelles"},{"Code": "SL", "Name": "Sierra Leone"},{"Code": "SG", "Name": "Singapore"},{"Code": "SX", "Name": "Sint Maarten (Dutch part)"},{"Code": "SK", "Name": "Slovakia"},{"Code": "SI", "Name": "Slovenia"},{"Code": "SB", "Name": "Solomon Islands"},{"Code": "SO", "Name": "Somalia"},{"Code": "ZA", "Name": "South Africa"},{"Code": "GS", "Name": "South Georgia and the South Sandwich Islands"},{"Code": "SS", "Name": "South Sudan"},{"Code": "ES", "Name": "Spain"},{"Code": "LK", "Name": "Sri Lanka"},{"Code": "SD", "Name": "Sudan"},{"Code": "SR", "Name": "Suriname"},{"Code": "SJ", "Name": "Svalbard and Jan Mayen"},{"Code": "SZ", "Name": "Swaziland"},{"Code": "SE", "Name": "Sweden"},{"Code": "CH", "Name": "Switzerland"},{"Code": "SY", "Name": "Syrian Arab Republic"},{"Code": "TW", "Name": "Taiwan, Province of China"},{"Code": "TJ", "Name": "Tajikistan"},{"Code": "TZ", "Name": "Tanzania, United Republic of"},{"Code": "TH", "Name": "Thailand"},{"Code": "TL", "Name": "Timor-Leste"},{"Code": "TG", "Name": "Togo"},{"Code": "TK", "Name": "Tokelau"},{"Code": "TO", "Name": "Tonga"},{"Code": "TT", "Name": "Trinidad and Tobago"},{"Code": "TN", "Name": "Tunisia"},{"Code": "TR", "Name": "Turkey"},{"Code": "TM", "Name": "Turkmenistan"},{"Code": "TC", "Name": "Turks and Caicos Islands"},{"Code": "TV", "Name": "Tuvalu"},{"Code": "UG", "Name": "Uganda"},{"Code": "UA", "Name": "Ukraine"},{"Code": "AE", "Name": "United Arab Emirates"},{"Code": "GB", "Name": "United Kingdom"},{"Code": "US", "Name": "United States"},{"Code": "UM", "Name": "United States Minor Outlying Islands"},{"Code": "UY", "Name": "Uruguay"},{"Code": "UZ", "Name": "Uzbekistan"},{"Code": "VU", "Name": "Vanuatu"},{"Code": "VE", "Name": "Venezuela, Bolivarian Republic of"},{"Code": "VN", "Name": "Viet Nam"},{"Code": "VG", "Name": "Virgin Islands, British"},{"Code": "VI", "Name": "Virgin Islands, U.S."},{"Code": "WF", "Name": "Wallis and Futuna"},{"Code": "EH", "Name": "Western Sahara"},{"Code": "YE", "Name": "Yemen"},{"Code": "ZM", "Name": "Zambia"},{"Code": "ZW", "Name": "Zimbabwe"}]';
+	var jsonData = JSON.parse(json);
+	for (var i = 0; i < jsonData.length; i++) {
+    	var item = jsonData[i];
+    	scrappers.push(scrapper(db, "https://m.youtube.com/feed/trending?&hl=en&client=mv-google&gl="+ item.Code, item.Code));
+	}
 	Promise.all(scrappers).then(function() {
 		cleanUp(db).then(function() {
 			readRows(db);
